b8a5b1851cc560fb88ff44fdfbab1826442f5cde
Dieudonne <iyabivuzed@gmail.com>
2025-03-12 17:06:02 +0100

    implemented the analytics of the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,33 +8,36 @@ import Quiz from "./pages/Quiz";
 import Results from "./pages/Results";
 import Statistics from "./pages/Statistics";
 import About from "./pages/About";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [quizResults, setQuizResults] = useState(null);
 
   return (
-        <BrowserRouter>
-    <div className="flex flex-col min-h-screen bg-gradient-to-b from-indigo-50 to-purple-50">
-      <h1>Hellow wordl</h1>
-      <Navbar />
-      <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route
-              path="/quiz"
-              element={<Quiz setQuizResults={setQuizResults} />}
-            />
-            <Route
-              path="/results"
-              element={<Results results={quizResults} />}
-            />
-            <Route path="/statistics" element={<Statistics />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-      </main>
-      <Footer />
-    </div>
-        </BrowserRouter>
+    <Analytics>
+      <BrowserRouter>
+        <div className="flex flex-col min-h-screen bg-gradient-to-b from-indigo-50 to-purple-50">
+          <h1>Hellow wordl</h1>
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/quiz"
+                element={<Quiz setQuizResults={setQuizResults} />}
+              />
+              <Route
+                path="/results"
+                element={<Results results={quizResults} />}
+              />
+              <Route path="/statistics" element={<Statistics />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </Analytics>
   );
 }
 
