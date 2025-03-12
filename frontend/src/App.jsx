af17981639036e723cf54e66a385b63aa739f1f1
Dieudonne <iyabivuzed@gmail.com>
2025-03-12 16:10:13 +0100

    fixed the issue of components not showing on the browser.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -13,22 +13,28 @@ function App() {
   const [quizResults, setQuizResults] = useState(null);
 
   return (
+        <BrowserRouter>
     <div className="flex flex-col min-h-screen bg-gradient-to-b from-indigo-50 to-purple-50">
+      <h1>Hellow wordl</h1>
       <Navbar />
       <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/quiz"
-            element={<Quiz setQuizResults={setQuizResults} />}
-          />
-          <Route path="/results" element={<Results results={quizResults} />} />
-          {/* <Route path="/statistics" element={<Statistics />} />
-          <Route path="/about" element={<About />} />  */}
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/quiz"
+              element={<Quiz setQuizResults={setQuizResults} />}
+            />
+            <Route
+              path="/results"
+              element={<Results results={quizResults} />}
+            />
+            {/* <Route path="/statistics" element={<Statistics />} />
+            <Route path="/about" element={<About />} /> */}
+          </Routes>
       </main>
       <Footer />
     </div>
+        </BrowserRouter>
   );
 }
 
